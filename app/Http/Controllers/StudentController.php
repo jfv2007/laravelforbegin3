20d38dc4c0cb310156b2964512d8cca67324d342
jesus feria villalobos <jfv61978@gmail.com>
2022-05-19 19:08:04 -0500

    primera actualizacion

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $student = Student::all();
+        return view('student.index', compact('student'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('student.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student = new Student;
+        $student->name=$request->input('name');
+        $student->email=$request->input('email');
+        $student->course=$request->input('course');
+        if($request->hasFile('profile_image'))
+        {
+            $file=$request->file('profile_image');
+            $extention=$file->getClientOriginalExtension();
+            $filename = time().'.'.$extention;
+            $file->move('uploads/students/',$filename);
+            $student->profile_image = $filename;
+        }
+        $student->save();
+        return redirect()->back()->with('status','Student Image Added Successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $student = Student::find($id);
+        return view('student.edit', compact('student'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $student = Student::find($id);
+        $student->name = $request->input('name');
+        $student->email = $request->input('email');
+        $student->course = $request->input('course');
+
+        if($request->hasfile('profile_image'))
+        {
+            $destination = 'uploads/students/'.$student->profile_image;
+            if(File::exists($destination))
+            {
+                File::delete($destination);
+            }
+            $file = $request->file('profile_image');
+            $extention = $file->getClientOriginalExtension();
+            $filename = time().'.'.$extention;
+            $file->move('uploads/students/', $filename);
+            $student->profile_image = $filename;
+        }
+
+        $student->update();
+        return redirect()->back()->with('status','Student Image Updated Successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        $destination = 'uploads/students/'.$student->profile_image;
+        if(File::exists($destination))
+        {
+            File::delete($destination);
+        }
+        $student->delete();
+        return redirect()->back()->with('status','Student Image Deleted Successfully');
+    }
+}
---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+            @if (session('status'))
+                <h6 class="alert alert-success">{{ session('status') }}</h6>
+            @endif
+
+            <div class="card">
+                <div class="card-header">
+                    <h4>Add estudiante con imagen
+                        <a href="{{ url('students') }}" class="btn btn-primary float-end">Regresar</a>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                    <form action="{{ url('add-student') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="">Student Name</label>
+                            <input type="text" name ="name" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Student Email</label>
+                            <input type="text" name="email" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Student Course</label>
+                            <input type="text" name="course" class="form-control">
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="">Student Profile Imagen</label>
+                            <input type="file" name="profile_image" class="form-control">
+                        </div>
+
+                        <div class="form-group mb-3">
+                            <button type="submit" class="btn btn-primary">Save Student</button>
+                        </div>
+
+                    </form>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
